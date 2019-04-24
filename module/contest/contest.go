b9817f76948a6bf9e40c8524075861d12fe4a8c3
limstash <limstash.w@gmail.com>
2019-04-24 16:30:43 +0800

    add module for query problems

--- module/contest/contest.go
+++ module/contest/contest.go
@@ -2,9 +2,11 @@ package contest
 
 import (
     "github.com/hytzongxuan/Codeforces-Hacker/module/http"
+    "github.com/bitly/go-simplejson"
     "encoding/json"
-    "errors"
     "net/http"
+    "errors"
+    "strconv"
 )
 
 type Contest struct {
@@ -23,6 +25,11 @@ type Contests struct {
     Result []Contest `json:"result"`
 }
 
+type Problem struct {
+    Index string
+    Name string
+}
+
 func queryContests (cookie *[]*http.Cookie) (string, error) {
     res, err := con.HttpGet("https://codeforces.com/api/contest.list?gym=false", cookie, map[string]string{"HOST":"codeforces.com"})
 
@@ -36,20 +43,67 @@ func queryContests (cookie *[]*http.Cookie) (string, error) {
 func GetContests (cookie *[]*http.Cookie) ([]Contest, error) {
     data, e := queryContests(cookie)
 
-    if(e != nil){
+    if e != nil {
         return nil, e
     }
 
     res := Contests{}
     e = json.Unmarshal([]byte(data), &res)
 
-    if(e != nil){
+    if e != nil {
         return nil, e
     }
 
-    if(res.Status != "OK"){
+    if res.Status != "OK" {
         return nil, errors.New("Codeforces Return Error Response")
     }
 
     return res.Result, nil
+}
+
+func queryProblems (contestID int, cookie *[]*http.Cookie) ([]byte, error){
+    res, err := con.HttpGetByte("https://codeforces.com/api/contest.standings?contestId="+strconv.Itoa(contestID)+"&from=1&count=1", cookie, map[string]string{"HOST":"codeforces.com"})
+
+    if err != nil {
+        return nil, err
+    }
+    
+    return res, nil   
+}
+
+func GetProblems (contestID int, cookie *[]*http.Cookie) ([]Problem, error){
+    data, e := queryProblems(contestID, cookie)
+
+    if e != nil {
+        return nil, e
+    }
+
+    res := []Problem{}
+    
+    js, e := simplejson.NewJson(data)
+
+    if e != nil {
+        return nil, e
+    }
+
+    status, e := js.Get("status").String()
+    
+    if status != "OK" {
+        return nil, errors.New("Codeforces Return Error Response")
+    }
+
+    problem, e := js.Get("result").Get("problems").Array()
+
+    if e != nil {
+        return nil, e
+    }
+
+    for i := 0; i < len(problem); i++ {
+        index := problem[i].(map[string]interface{})["index"].(string)
+        name := problem[i].(map[string]interface{})["name"].(string)
+
+        res = append(res, Problem{index, name})
+    }
+
+    return res, nil
 }
--- module/contest/contest_test.go
+++ module/contest/contest_test.go
@@ -9,9 +9,10 @@ func Test_QueryContests(t *testing.T) {
 	var GlobalCookie []*http.Cookie
 	i, e := queryContests(&GlobalCookie)
 	
-	if i == "" || e != nil {
+	if e != nil {
 		t.Error(e)
-		t.Error("QueryContests Test Failed")
+	}else if i == "" {
+		t.Error("Empty Response from codeforces.com")
 	}else if GlobalCookie == nil || len(GlobalCookie) == 0{
 		t.Error("Cookie is en empty field")
 	}
@@ -39,5 +40,44 @@ func Test_GetContests(t *testing.T){
 		t.Log("Get Contests Test Failed")
 	}else{
 		t.Log("Get Contests Test Passed")
+	}
+}
+
+func Test_QueryProblems(t *testing.T){
+	var GlobalCookie []*http.Cookie
+
+	i, e := queryProblems(556, &GlobalCookie)
+
+	if e != nil {
+		t.Error(e)
+	}else if i == nil || len(i) == 0{
+		t.Error("Empty Response from codeforces.com")
+	}else if GlobalCookie == nil || len(GlobalCookie) == 0{
+		t.Error("Cookie is en empty field")
+	}
+
+	if e != nil || i == nil || len(i) == 0 || GlobalCookie == nil || len(GlobalCookie) == 0{
+		t.Log("Query Problems Test Failed")
+	}else{
+		t.Log("Query Problems Test Passed")
+	}
+}
+
+func Test_GetProblems(t *testing.T){
+	var GlobalCookie []*http.Cookie
+	i, e := GetProblems(556, &GlobalCookie)
+
+	if e != nil {
+		t.Error(e)
+	}else if i == nil || len(i) == 0{
+		t.Error("Problems is an empty field")
+	}else if GlobalCookie == nil || len(GlobalCookie) == 0{
+		t.Error("Cookie is en empty field")
+	}
+
+	if e != nil || i == nil || len(i) == 0 || GlobalCookie == nil || len(GlobalCookie) == 0{
+		t.Log("Get Problems Test Failed")
+	}else{
+		t.Log("Get Problems Test Passed")
 	}
 }
