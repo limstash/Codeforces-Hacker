2f5dd1e0c496d2a3ef04cf9c8ea0dfc687c42dd9
limstash <limstash.w@gmail.com>
2019-04-25 01:01:46 +0800

    add new unit tests

--- module/contest/contest_test.go
+++ module/contest/contest_test.go
@@ -9,15 +9,20 @@ func Test_QueryContests(t *testing.T) {
 	var GlobalCookie []*http.Cookie
 	i, e := queryContests(&GlobalCookie)
 	
+	status := true
+
 	if e != nil {
 		t.Error(e)
+		status = false
 	}else if i == "" {
 		t.Error("Empty Response from codeforces.com")
+		status = false
 	}else if GlobalCookie == nil || len(GlobalCookie) == 0{
 		t.Error("Cookie is en empty field")
+		status = false
 	}
 
-	if e != nil || i == "" || GlobalCookie == nil || len(GlobalCookie) == 0 {
+	if status == false {
 		t.Log("Query Contests Test Failed")
 	}else{
 		t.Log("Query Contests Test Passed")
@@ -28,15 +33,20 @@ func Test_GetContests(t *testing.T){
 	var GlobalCookie []*http.Cookie
 	i, e := GetContests(&GlobalCookie)
 
+	status := true
+
 	if e != nil {
 		t.Error(e)
+		status = false
 	}else if i == nil {
 		t.Error("Result is an empty field")
+		status = false
 	}else if GlobalCookie == nil || len(GlobalCookie) == 0{
 		t.Error("Cookie is en empty field")
+		status = false
 	}
 
-	if e != nil || i == nil || GlobalCookie == nil || len(GlobalCookie) == 0{
+	if status == false {
 		t.Log("Get Contests Test Failed")
 	}else{
 		t.Log("Get Contests Test Passed")
@@ -48,15 +58,20 @@ func Test_QueryProblems(t *testing.T){
 
 	i, e := queryProblems(556, &GlobalCookie)
 
+	status := true
+
 	if e != nil {
 		t.Error(e)
+		status = false
 	}else if i == nil || len(i) == 0{
 		t.Error("Empty Response from codeforces.com")
+		status = false
 	}else if GlobalCookie == nil || len(GlobalCookie) == 0{
 		t.Error("Cookie is en empty field")
+		status = false
 	}
 
-	if e != nil || i == nil || len(i) == 0 || GlobalCookie == nil || len(GlobalCookie) == 0{
+	if status == false {
 		t.Log("Query Problems Test Failed")
 	}else{
 		t.Log("Query Problems Test Passed")
@@ -67,15 +82,20 @@ func Test_GetProblems(t *testing.T){
 	var GlobalCookie []*http.Cookie
 	i, e := GetProblems(556, &GlobalCookie)
 
+	status := true
+
 	if e != nil {
 		t.Error(e)
+		status = false
 	}else if i == nil || len(i) == 0{
 		t.Error("Problems is an empty field")
+		status = false
 	}else if GlobalCookie == nil || len(GlobalCookie) == 0{
 		t.Error("Cookie is en empty field")
+		status = false
 	}
 
-	if e != nil || i == nil || len(i) == 0 || GlobalCookie == nil || len(GlobalCookie) == 0{
+	if status == false {
 		t.Log("Get Problems Test Failed")
 	}else{
 		t.Log("Get Problems Test Passed")
--- module/http/http_test.go
+++ module/http/http_test.go
@@ -7,17 +7,22 @@ import (
 
 func Test_httpGET(t *testing.T){
 	var GlobalCookie []*http.Cookie
-	http_res, http_err := HttpGet("http://www.baidu.com", &GlobalCookie, nil)
+	http_res, http_err := HttpGet("http://www.baidu.com", &GlobalCookie, map[string]string{"Host" : "www.baidu.com"})
+
+	status := true
 
 	if http_err != nil {
 		t.Error(http_err)
+		status = false
 	}else if http_res == "" {
 		t.Error("No Response from http://www.baidu.com")
+		status = false
 	}else if GlobalCookie == nil || len(GlobalCookie) == 0 {
 		t.Error("Cookie is an empty field")
+		status = false
 	}
 
-	if http_err != nil || http_res == "" || GlobalCookie == nil || len(GlobalCookie) == 0 {
+	if status == false {
 		t.Log("HTTP GET Query Test Failed")
 	}else{
 		t.Log("HTTP GET Query Test Passed")
@@ -26,17 +31,22 @@ func Test_httpGET(t *testing.T){
 
 func Test_httpsGET(t *testing.T){
 	var GlobalCookie []*http.Cookie
-	https_res, https_err := HttpGet("http://www.baidu.com", &GlobalCookie, nil)
+	https_res, https_err := HttpGet("http://www.baidu.com", &GlobalCookie, map[string]string{"Host" : "www.baidu.com"})
+
+	status := true
 
 	if https_err != nil {
 		t.Error(https_err)
+		status = false
 	}else if https_res == "" {
 		t.Error("No Response from http://www.baidu.com")
+		status = false
 	}else if GlobalCookie == nil || len(GlobalCookie) == 0 {
 		t.Error("Cookie is an empty field")
+		status = false
 	}
 
-	if https_err != nil || https_res == "" || GlobalCookie == nil || len(GlobalCookie) == 0 {
+	if status == false {
 		t.Log("HTTPS GET Query Test Failed")
 	}else{
 		t.Log("HTTPS GET Query Test Passed")
@@ -45,17 +55,22 @@ func Test_httpsGET(t *testing.T){
 
 func Test_httpPOST(t *testing.T){
 	var GlobalCookie []*http.Cookie
-	http_res, http_err := HttpPost("http://www.baidu.com", &GlobalCookie, nil, nil)
+	http_res, http_err := HttpPost("http://www.baidu.com", &GlobalCookie, map[string]string{"Host" : "www.baidu.com"}, map[string]string{"hi" : "hi"})
+
+	status := true
 
 	if http_err != nil {
 		t.Error(http_err)
+		status = false
 	}else if http_res == "" {
 		t.Error("No Response from http://www.baidu.com")
+		status = false
 	}else if GlobalCookie == nil || len(GlobalCookie) == 0 {
 		t.Error("Cookie is an empty field")
+		status = false
 	}
 
-	if http_err != nil || http_res == "" || GlobalCookie == nil || len(GlobalCookie) == 0 {
+	if status == false {
 		t.Log("HTTP GET Query Test Failed")
 	}else{
 		t.Log("HTTP GET Query Test Passed")
@@ -64,17 +79,22 @@ func Test_httpPOST(t *testing.T){
 
 func Test_httpsPOST(t *testing.T){
 	var GlobalCookie []*http.Cookie
-	https_res, https_err := HttpPost("http://www.baidu.com", &GlobalCookie, nil, nil)
+	https_res, https_err := HttpPost("http://www.baidu.com", &GlobalCookie, map[string]string{"Host" : "www.baidu.com"}, map[string]string{"hi" : "hi"})
+
+	status := true
 
 	if https_err != nil {
 		t.Error(https_err)
+		status = false
 	}else if https_res == "" {
 		t.Error("No Response from http://www.baidu.com")
+		status = false
 	}else if GlobalCookie == nil || len(GlobalCookie) == 0 {
 		t.Error("Cookie is an empty field")
+		status = false
 	}
 
-	if https_err != nil || https_res == "" || GlobalCookie == nil || len(GlobalCookie) == 0 {
+	if 	status == false{
 		t.Log("HTTPS GET Query Test Failed")
 	}else{
 		t.Log("HTTPS GET Query Test Passed")
