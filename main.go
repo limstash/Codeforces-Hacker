92372cc00f080c84c4be68be49d9f32b7ce4dfa6
hytzongxuan <hytzongxuan@gmail.com>
2019-04-24 17:03:53 +0800

    add module problem choose

--- main.go
+++ main.go
@@ -13,19 +13,28 @@ func version(){
 var Cookie []*http.Cookie
 
 func main(){
+	version()
+
 	Contest, CSRF, e := app.Load(&Cookie)
 
+	if e != nil || CSRF == "" {
+		fmt.Println(e);
+		return;
+	}
+
+	contestID, e := app.FindContest(Contest)
+
+	if e != nil{
+		fmt.Println(e);
+		return;
+	}
+
+	id, e := app.ChooseProblem(contestID, &Cookie)
+
 	if e != nil {
 		fmt.Println(e);
 		return;
 	}
 
-	e = app.FindContest(Contest)
-
-	if e != nil {
-		fmt.Println(e);
-		return;
-	}
-
-	fmt.Println(CSRF)
+	panic(id)
 }
