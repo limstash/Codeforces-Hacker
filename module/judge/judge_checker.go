fda68bbd064062dd96a9e4a05cd59a753650c9da
hytzongxuan <hytzongxuan@gmail.com>
2019-04-26 20:40:20 +0800

    add module "judge on linux"

---
+++ module/judge/judge_checker.go
@@ -0,0 +1,106 @@
+package judge
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Diff(SubmissionID int) bool {
+	SubmissionPath := getPath() + "/src/" + strconv.Itoa(SubmissionID)
+	status := true
+
+	outputFile, err := os.Open(SubmissionPath + "/data.out")
+
+	if err != nil {
+		return false
+	}
+
+	standFile, err := os.Open(SubmissionPath + "/data.ans")
+
+	if err != nil {
+		return false
+	}
+
+	outputBuff := bufio.NewReader(outputFile)
+	standBuff := bufio.NewReader(standFile)
+
+	end1 := false
+	end2 := false
+
+	for {
+		var output string
+		var stand string
+
+		if end1 == false {
+			a, _, c := outputBuff.ReadLine()
+
+			if c == io.EOF {
+				end1 = true
+			}
+
+			output = string(a)
+
+			if end2 == true && output != "" && output != "\r" && output != "\n" && output != "\r\n" {
+				fmt.Println("yes1")
+				status = false
+			}
+		}
+
+		if end2 == false {
+			b, _, d := standBuff.ReadLine()
+
+			if d == io.EOF {
+				end2 = true
+			}
+
+			stand = string(b)
+
+			if end1 == true && stand != "" && stand != "\r" && stand != "\n" && stand != "\r\n" {
+				status = false
+			}
+		}
+
+		if end1 == true && end2 == true {
+			break
+		}
+
+		for i := 0; i < min(len(output), len(stand)); i++ {
+			if output[i] != stand[i] {
+				status = false
+				break
+			}
+		}
+
+		if len(output) < len(stand) {
+			for i := len(output); i < len(stand); i++ {
+				if stand[i] != ' ' && stand[i] != '\r' && stand[i] != '\n' {
+					status = false
+					break
+				}
+			}
+		} else {
+			for i := len(stand); i < len(output); i++ {
+				if output[i] != ' ' && output[i] != '\r' && output[i] != '\n' {
+					status = false
+					break
+				}
+			}
+		}
+
+		if status == false {
+			return status
+		}
+	}
+
+	return status
+}
---
+++ module/judge/judge_runUnix.go
@@ -0,0 +1,176 @@
+package judge
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+)
+
+func RunUnixBin(SubmissionID int, SubmissionPath string) bool {
+	subProcess := exec.Command(SubmissionPath + "/main")
+
+	stdin, err := subProcess.StdinPipe()
+
+	if err != nil {
+		return false
+	}
+
+	defer stdin.Close()
+
+	stdout, err := os.OpenFile(SubmissionPath+"/data.out", os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer stdout.Close()
+
+	subProcess.Stdout = stdout
+	subProcess.Stderr = os.Stderr
+
+	if err = subProcess.Start(); err != nil {
+		return false
+	}
+
+	fi, err := os.Open(SubmissionPath + "/data.in")
+
+	if err != nil {
+		return false
+	}
+
+	defer fi.Close()
+
+	inputBuff := bufio.NewReader(fi)
+
+	for {
+		a, _, c := inputBuff.ReadLine()
+		if c == io.EOF {
+			break
+		}
+		io.WriteString(stdin, string(a)+"\n")
+	}
+
+	subProcess.Wait()
+
+	return true
+}
+
+func RunPython2(SubmissionID int, SubmissionPath string) bool {
+	subProcess := exec.Command("python2", SubmissionPath+"/main.py")
+
+	stdin, err := subProcess.StdinPipe()
+
+	if err != nil {
+		return false
+	}
+
+	defer stdin.Close()
+
+	stdout, err := os.OpenFile(SubmissionPath+"/data.out", os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer stdout.Close()
+
+	subProcess.Stdout = stdout
+
+	if err = subProcess.Start(); err != nil {
+		return false
+	}
+
+	fi, err := os.Open(SubmissionPath + "/data.in")
+
+	if err != nil {
+		return false
+	}
+
+	defer fi.Close()
+
+	inputBuff := bufio.NewReader(fi)
+
+	for {
+		a, _, c := inputBuff.ReadLine()
+		if c == io.EOF {
+			break
+		}
+		io.WriteString(stdin, string(a)+"\n")
+	}
+
+	subProcess.Wait()
+
+	return true
+}
+
+func RunPython3(SubmissionID int, SubmissionPath string) bool {
+	subProcess := exec.Command("python3", SubmissionPath+"/main.py")
+
+	stdin, err := subProcess.StdinPipe()
+
+	if err != nil {
+		return false
+	}
+
+	defer stdin.Close()
+
+	stdout, err := os.OpenFile(SubmissionPath+"/data.out", os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer stdout.Close()
+
+	subProcess.Stdout = stdout
+
+	if err = subProcess.Start(); err != nil {
+		return false
+	}
+
+	fi, err := os.Open(SubmissionPath + "/data.in")
+
+	if err != nil {
+		return false
+	}
+
+	defer fi.Close()
+
+	inputBuff := bufio.NewReader(fi)
+
+	for {
+		a, _, c := inputBuff.ReadLine()
+		if c == io.EOF {
+			break
+		}
+		io.WriteString(stdin, string(a)+"\n")
+	}
+
+	subProcess.Wait()
+
+	return true
+}
+
+func RunUnixCode(SubmissionID int, lang string) bool {
+	SubmissionPath := getPath() + "/src/" + strconv.Itoa(SubmissionID)
+
+	result := true
+
+	switch lang {
+	case "GNU C11":
+		result = RunUnixBin(SubmissionID, SubmissionPath)
+	case "GNU C++11":
+		result = RunUnixBin(SubmissionID, SubmissionPath)
+	case "GNU C++14":
+		result = RunUnixBin(SubmissionID, SubmissionPath)
+	case "GNU C++17":
+		result = RunUnixBin(SubmissionID, SubmissionPath)
+	case "Go":
+		result = RunUnixBin(SubmissionID, SubmissionPath)
+	case "Python 2":
+		result = RunPython2(SubmissionID, SubmissionPath)
+	case "Python 3":
+		result = RunPython3(SubmissionID, SubmissionPath)
+	default:
+		result = true
+	}
+
+	return result
+}
