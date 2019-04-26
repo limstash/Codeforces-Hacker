d11c8cfd5e0b03b134ae77c355335905a925470f
limstash <limstash.w@gmail.com>
2019-04-26 21:07:58 +0800

    update module "code" unit test

--- module/code/code_test.go
+++ module/code/code_test.go
@@ -2,6 +2,8 @@ package code
 
 import (
 	"net/http"
+	"os"
+	"runtime"
 	"testing"
 
 	"github.com/hytzongxuan/Codeforces-Hacker/module/token"
@@ -37,5 +39,78 @@ func Test_QueryCode(t *testing.T) {
 		t.Log("Query Code Test Failed")
 	} else {
 		t.Log("Query Code Test Passed")
+	}
+}
+
+func Test_Save(t *testing.T) {
+
+	if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
+		os.MkdirAll(getPath()+"/src/53319035", 0777)
+	} else {
+		os.MkdirAll(getPath()+"\\src\\53319035", 0777)
+	}
+
+	status := true
+
+	e := SaveCode(53319035, "GNU C++11", "test")
+
+	if e != nil {
+		status = false
+		t.Error("Module code fail at saving GNU C++11 Code")
+		t.Error(e)
+	}
+
+	e = SaveCode(53319035, "GNU C++14", "test")
+
+	if e != nil {
+		status = false
+		t.Error("Module code fail at saving GNU C++14 Code")
+		t.Error(e)
+	}
+
+	e = SaveCode(53319035, "GNU C++17", "test")
+
+	if e != nil {
+		status = false
+		t.Error("Module code fail at saving GNU C++17 Code")
+		t.Error(e)
+	}
+
+	e = SaveCode(53319035, "GNU C11", "test")
+
+	if e != nil {
+		status = false
+		t.Error("Module code fail at saving GNU C11 Code")
+		t.Error(e)
+	}
+
+	e = SaveCode(53319035, "Python2", "test")
+
+	if e != nil {
+		status = false
+		t.Error("Module code fail at saving Python2 Code")
+		t.Error(e)
+	}
+
+	e = SaveCode(53319035, "Python3", "test")
+
+	if e != nil {
+		status = false
+		t.Error("Module code fail at saving Python3 Code")
+		t.Error(e)
+	}
+
+	e = SaveCode(53319035, "Go", "test")
+
+	if e != nil {
+		status = false
+		t.Error("Module code fail at saving Go Code")
+		t.Error(e)
+	}
+
+	if status == true {
+		t.Log("Save Code Test Passed")
+	} else {
+		t.Log("Save Code Test Failed")
 	}
 }
