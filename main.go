03db268b08fe552ae4fb2ac6b14456cff61a0601
hytzongxuan <hytzongxuan@gmail.com>
2019-04-30 13:56:11 +0800

    add function "login and save testdatas"

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/hytzongxuan/Codeforces-Hacker/app"
 )
@@ -14,29 +15,54 @@ func version() {
 // Cookie are the HTTP Cookies which saved from https://codeforces.com
 var Cookie []*http.Cookie
 
-func main() {
-	version()
+func finish() {
+	time.Sleep(time.Millisecond * 5000)
+}
 
+func main() {
+
+	version()
 	Contest, CSRF, e := app.Load(&Cookie)
 
 	if e != nil || CSRF == "" {
 		fmt.Println(e)
-		return
+		finish()
 	}
 
 	contestID, e := app.FindContest(Contest)
 
 	if e != nil {
 		fmt.Println(e)
-		return
+		finish()
 	}
 
 	id, e := app.ChooseProblem(contestID, &Cookie)
 
 	if e != nil {
 		fmt.Println(e)
-		return
+		finish()
 	}
 
-	panic(id)
+	loginChoose, e := app.QueryLoginChoose()
+
+	if e != nil {
+		fmt.Println(e)
+		finish()
+	}
+
+	if loginChoose == true {
+		hackChoose, e := app.QueryHackChoose()
+
+		if e != nil {
+			fmt.Println(e)
+			finish()
+		}
+
+		app.Login(&Cookie, CSRF)
+		fmt.Println(hackChoose)
+	}
+
+	app.SaveData()
+	fmt.Println(id)
+
 }
