c0e20240a75da8d175dbc25b69f8fa0193a9bf09
limstash <limstash.w@gmail.com>
2019-06-25 21:39:24 +0800

    using config.json instead of stdin

--- main.go
+++ main.go
@@ -1,104 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"net/http"
-	"os"
-	"strconv"
-	"time"
 
 	"github.com/hytzongxuan/Codeforces-Hacker/app"
 )
 
 func version() {
-	fmt.Println("Codeforces Hacker v0.2")
+	fmt.Println("Codeforces Hacker v0.3 by hytzongxuan")
 }
 
-// Cookie are the HTTP Cookies which saved from https://codeforces.com
-var Cookie []*http.Cookie
-
-func finish() {
-	time.Sleep(time.Millisecond * 5000)
-	os.Exit(0)
-}
+var configFilePath string
+var remoteServerURL string
 
 func main() {
-
 	version()
-	Contest, CSRF, e := app.Load(&Cookie)
 
-	if e != nil || CSRF == "" {
-		fmt.Println(e)
-		finish()
-	}
+	flag.StringVar(&configFilePath, "c", "./config.json", "config file")
+	flag.StringVar(&remoteServerURL, "s", "https://codeforces.com", "remote server URL")
 
-	contestInfo, e := app.FindContest(Contest)
+	flag.Parse()
 
-	if e != nil {
-		fmt.Println(e)
-		finish()
-	}
-
-	choose, e := app.ChooseProblem(contestInfo.ID, &Cookie)
-
-	if e != nil {
-		fmt.Println(e)
-		finish()
-	}
-
-	loginChoose, e := app.QueryLoginChoose()
-
-	if e != nil {
-		fmt.Println(e)
-		finish()
-	}
-
-	var hackChoose bool
-
-	if loginChoose == true {
-		hackChoose, e = app.QueryHackChoose()
-
-		if e != nil {
-			fmt.Println(e)
-			finish()
-		}
-
-		app.Login(&Cookie, CSRF)
-
-		fmt.Println("[Info] Refetching CSRF token...")
-		CSRF, e = app.GetCSRF(&Cookie)
-
-		if e != nil {
-			fmt.Println(e)
-			finish()
-		}
-	}
-
-	app.SaveData()
-
-	fmt.Println("[Info] Fetching submissions...")
-	submit := app.GetSubmission(&Cookie, contestInfo, choose)
-	fmt.Println("[Info] Fetching submissions successful")
-
-	fmt.Println(" ")
-
-	if hackChoose == true {
-		fmt.Println("[Info] Not Support auto hack")
-	}
-
-	for i := 0; i < len(submit); i++ {
-		flag, e := app.TestCode(submit[i].SubmissionID, submit[i].Language, false, &Cookie, CSRF)
-
-		if flag == true {
-			fmt.Println("[Info] Submission " + strconv.Itoa(submit[i].SubmissionID) + " Accepted")
-		}
-
-		if e != nil {
-			if hackChoose == true {
-
-			} else {
-
-			}
-		}
-	}
+	app.Load(configFilePath, remoteServerURL)
 }
