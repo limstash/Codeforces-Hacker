283a084cb43f200030de12ad90452587b4b9ba1a
limstash <limstash.w@gmail.com>
2019-06-26 11:02:43 +0800

    update module conn

--- module/conn/conn.go
+++ module/conn/conn.go
@@ -7,6 +7,8 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+
+	. "github.com/hytzongxuan/Codeforces-Hacker/common"
 )
 
 func redirectRules(req *http.Request, via []*http.Request) error {
@@ -16,82 +18,27 @@ func redirectRules(req *http.Request, via []*http.Request) error {
 	return nil
 }
 
-func httpGetQuery(uri string, cookie *[]*http.Cookie, header map[string]string, isRedirect bool) ([]byte, bool, error) {
-	var client *http.Client
-
-	if isRedirect {
-		client = &http.Client{
-			CheckRedirect: redirectRules,
-		}
-	} else {
-		client = &http.Client{}
-	}
-
-	var redirectStatus bool
-
-	req, err := http.NewRequest("GET", uri, nil)
-
-	if err != nil {
-		return nil, false, err
-	}
-
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3")
+func addCommonHeader(req *http.Request) {
 	req.Header.Set("Accept-Encoding", "none")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7")
+	req.Header.Set("Accept-Language", "en-US")
 	req.Header.Set("Cache-Control", "max-age=0")
-	req.Header.Set("User-Agent", "Mozilla/5.s0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36")
+}
 
+func addExtraHeader(header map[string]string, req *http.Request) {
 	for headerName, headerValue := range header {
 		req.Header.Set(headerName, headerValue)
 	}
-
-	for _, v := range *cookie {
-		req.AddCookie(v)
-	}
-
-	resp, err := client.Do(req)
-
-	if err != nil {
-		flysnowRegexp := regexp.MustCompile(`No Redirect`)
-		params := flysnowRegexp.FindStringSubmatch(err.Error())
-
-		if len(params) == 0 {
-			return nil, false, err
-		} else {
-			redirectStatus = true
-		}
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if resp.StatusCode >= 400 {
-		return nil, false, errors.New("Return HTTP Code " + strconv.Itoa(resp.StatusCode))
-	}
-
-	newCookie := resp.Cookies()
-
-	for i := 0; i < len(newCookie); i++ {
-		*cookie = append(*cookie, newCookie[i])
-	}
-
-	return body, redirectStatus, nil
 }
 
-func httpPostQuery(uri string, cookie *[]*http.Cookie, header map[string]string, data map[string]string, isRedirect bool) ([]byte, bool, error) {
-	var client *http.Client
-
-	if isRedirect {
-		client = &http.Client{
-			CheckRedirect: redirectRules,
-		}
-	} else {
-		client = &http.Client{}
+func addExtraCookie(authentication *Authentication, req *http.Request) {
+	for _, v := range authentication.Cookie {
+		req.AddCookie(v)
 	}
+}
 
+func addPostBody(data map[string]string) string {
 	var postdata http.Request
-	var redirectStatus bool
 
 	postdata.ParseForm()
 
@@ -100,26 +47,42 @@ func httpPostQuery(uri string, cookie *[]*http.Cookie, header map[string]string,
 	}
 
 	bodystr := strings.TrimSpace(postdata.Form.Encode())
-	req, err := http.NewRequest("POST", uri, strings.NewReader(bodystr))
+	return bodystr
+}
+
+func httpRequest(request Request) (Response, error) {
+	var response = Response{}
+	var client *http.Client
+
+	if request.NotRedirect {
+		client = &http.Client{
+			CheckRedirect: redirectRules,
+		}
+	} else {
+		client = &http.Client{}
+	}
+
+	var bodystr string
+
+	if request.Method == "POST" {
+		bodystr = addPostBody(request.Data)
+	}
+
+	req, err := http.NewRequest(request.Method, request.URL, strings.NewReader(bodystr))
 
 	if err != nil {
-		return nil, false, err
+		return response, err
+	}
+
+	addCommonHeader(req)
+	addExtraHeader(request.Header, req)
+	addExtraCookie(request.Authentication, req)
+
+	if request.Method == "POST" {
+		req.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	} else {
-		redirectStatus = true
-	}
-
-	req.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
-	req.Header.Set("Accept-Encoding", "none")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7")
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("User-Agent", "Mozilla/5.s0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36")
-
-	for headerName, headerValue := range header {
-		req.Header.Set(headerName, headerValue)
-	}
-
-	for _, v := range *cookie {
-		req.AddCookie(v)
+		req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3")
 	}
 
 	resp, err := client.Do(req)
@@ -129,12 +92,18 @@ func httpPostQuery(uri string, cookie *[]*http.Cookie, header map[string]string,
 		params := flysnowRegexp.FindStringSubmatch(err.Error())
 
 		if len(params) == 0 {
-			return nil, false, err
+			return response, err
+		} else {
+			response.RedirectStatus = true
 		}
 	}
 
 	if resp.StatusCode >= 400 {
-		return nil, false, errors.New("Return HTTP Code " + strconv.Itoa(resp.StatusCode))
+		return response, errors.New("Remote server return with code " + strconv.Itoa(resp.StatusCode))
+	}
+
+	if response.RedirectStatus {
+		return response, nil
 	}
 
 	defer resp.Body.Close()
@@ -142,62 +111,32 @@ func httpPostQuery(uri string, cookie *[]*http.Cookie, header map[string]string,
 	body, err := ioutil.ReadAll(resp.Body)
 
 	if err != nil {
-		return nil, false, err
+		return response, err
 	}
 
 	newCookie := resp.Cookies()
 
 	for i := 0; i < len(newCookie); i++ {
-		*cookie = append(*cookie, newCookie[i])
+		request.Authentication.Cookie = append(request.Authentication.Cookie, newCookie[i])
 	}
 
-	return body, redirectStatus, nil
+	response.RedirectStatus = false
+	response.ResponseBody = body
+
+	return response, nil
 }
 
-// HTTPGetByte will send a GET query to remote server and return in []byte
-func HTTPGetByte(uri string, cookie *[]*http.Cookie, header map[string]string) ([]byte, error) {
-	a, _, b := httpGetQuery(uri, cookie, header, false)
-	return a, b
-}
+func HTTPRequest(request Request) (Response, error) {
+	var response Response
+	var err error
 
-// HTTPPostByte will send a POST query to remote server and return in []byte
-func HTTPPostByte(uri string, cookie *[]*http.Cookie, header map[string]string, data map[string]string) ([]byte, error) {
-	a, _, b := httpPostQuery(uri, cookie, header, data, false)
-	return a, b
-}
+	for i := 1; i <= 3; i++ {
+		response, err = httpRequest(request)
 
-// HTTPGetByteNR will send a GET query to remote server without redirect and return in []byte
-func HTTPGetByteNR(uri string, cookie *[]*http.Cookie, header map[string]string) ([]byte, bool, error) {
-	a, b, c := httpGetQuery(uri, cookie, header, true)
-	return a, b, c
-}
+		if err == nil {
+			return response, nil
+		}
+	}
 
-// HTTPPostByteNR will send a POST query to remote server without redirect and return in []byte
-func HTTPPostByteNR(uri string, cookie *[]*http.Cookie, header map[string]string, data map[string]string) ([]byte, bool, error) {
-	a, b, c := httpPostQuery(uri, cookie, header, data, true)
-	return a, b, c
-}
-
-// HTTPGet will send a Get query to remote server and return in string
-func HTTPGet(uri string, cookie *[]*http.Cookie, header map[string]string) (string, error) {
-	a, b := HTTPGetByte(uri, cookie, header)
-	return string(a), b
-}
-
-// HTTPPost will send a POST query to remote server and return in string
-func HTTPPost(uri string, cookie *[]*http.Cookie, header map[string]string, data map[string]string) (string, error) {
-	a, b := HTTPPostByte(uri, cookie, header, data)
-	return string(a), b
-}
-
-// HTTPGetNR will send a Get query to remote server without redirect and return in string
-func HTTPGetNR(uri string, cookie *[]*http.Cookie, header map[string]string) (string, bool, error) {
-	a, status, b := HTTPGetByteNR(uri, cookie, header)
-	return string(a), status, b
-}
-
-// HTTPPostNR will send a POST query to remote server without redirect and return in string
-func HTTPPostNR(uri string, cookie *[]*http.Cookie, header map[string]string, data map[string]string) (string, bool, error) {
-	a, status, b := HTTPPostByteNR(uri, cookie, header, data)
-	return string(a), status, b
+	return response, err
 }
--- module/conn/conn_test.go
+++ module/conn/conn_test.go
@@ -1,102 +1,276 @@
 package conn
 
 import (
-	"net/http"
 	"testing"
+
+	"github.com/bitly/go-simplejson"
+	. "github.com/hytzongxuan/Codeforces-Hacker/common"
 )
 
-func Test_httpGET(t *testing.T) {
-	var GlobalCookie []*http.Cookie
+var authentication Authentication
 
-	res, e := HTTPGet("http://www.bing.com", &GlobalCookie, map[string]string{"Host": "www.bing.com"})
+func testPOST01(t *testing.T) bool {
+	request := Request{}
 
+	request.URL = "https://api.yellowfish.top/Codeforces-Hacker/post-test.php"
+	request.Method = "POST"
+	request.NotRedirect = false
+	request.Authentication = &authentication
+	request.Data = map[string]string{"post-data01": "test01", "post-data02": "test02"}
+
+	response, err := httpRequest(request)
+
+	if err != nil {
+		t.Error(err)
+		return false
+	}
+
+	js, err := simplejson.NewJson(response.ResponseBody)
+
+	if err != nil {
+		t.Skip("Something goes wrong, Skipped")
+		return true
+	}
+
+	postdata01 := js.Get("post-data01").MustString()
+	postdata02 := js.Get("post-data02").MustString()
+
+	if postdata01 != "test01" || postdata02 != "test02" {
+		t.Error("Test Failed: (Case 01) POST body not exists or format error")
+		return false
+	}
+
+	if response.RedirectStatus == true {
+		t.Error("Test Failed: (Case 01) Redirect should not be found here")
+		return false
+	}
+
+	return true
+}
+
+func testPOST02(t *testing.T) bool {
+	request := Request{}
+
+	request.URL = "https://api.yellowfish.top/Codeforces-Hacker/not-found.php"
+	request.Method = "POST"
+	request.NotRedirect = false
+	request.Authentication = &authentication
+
+	_, err := httpRequest(request)
+
+	if err == nil || err.Error() != "Remote server return with code 404" {
+		t.Error("Test Failed: (Case 02) httpRequest should throw an error here (http 404)")
+		return false
+	}
+
+	return true
+}
+
+func testPOST03(t *testing.T) bool {
+	request := Request{}
+
+	request.URL = "https://api.yellowfish.top/Codeforces-Hacker/"
+	request.Method = "POST"
+	request.NotRedirect = false
+	request.Authentication = &authentication
+
+	_, err := httpRequest(request)
+
+	if err == nil || err.Error() != "Remote server return with code 403" {
+		t.Error("Test Failed: (Case 03) httpRequest should throw an error here (http 403)")
+		return false
+	}
+
+	return true
+}
+
+func testPOST04(t *testing.T) bool {
+	request := Request{}
+
+	request.URL = "https://api.yellowfish.top/Codeforces-Hacker/cookie-test.php"
+	request.Method = "POST"
+	request.NotRedirect = false
+	request.Authentication = &authentication
+
+	_, err := httpRequest(request)
+
+	if err != nil {
+		t.Error(err)
+		return false
+	}
+
+	response, err := httpRequest(request)
+
+	js, err := simplejson.NewJson(response.ResponseBody)
+
+	if err != nil {
+		t.Skip("Something goes wrong, Skipped")
+		return true
+	}
+
+	cookiedata01 := js.Get("cookie-test01").MustString()
+	cookiedata02 := js.Get("cookie-test02").MustString()
+
+	if cookiedata01 != "test01" || cookiedata02 != "test02" {
+		t.Error("Test Failed: (Case 04) Cookie not exists or format error")
+		return false
+	}
+
+	if response.RedirectStatus == true {
+		t.Error("Test Failed: (Case 04) Redirect should not be found here")
+		return false
+	}
+
+	return true
+}
+
+func testGet05(t *testing.T) bool {
+	request := Request{}
+
+	request.URL = "https://api.yellowfish.top/Codeforces-Hacker/header-test.php"
+	request.Method = "GET"
+	request.NotRedirect = false
+	request.Authentication = &authentication
+	request.Header = map[string]string{"Header-Data01": "test01", "Header-Data02": "test02"}
+
+	response, err := httpRequest(request)
+
+	if err != nil {
+		t.Error(err)
+		return false
+	}
+
+	js, err := simplejson.NewJson(response.ResponseBody)
+
+	if err != nil {
+		t.Skip("Something goes wrong, Skipped")
+		return true
+	}
+
+	headerdata01 := js.Get("Header-Data01").MustString()
+	headerdata02 := js.Get("Header-Data02").MustString()
+
+	if headerdata01 != "test01" || headerdata02 != "test02" {
+		t.Error("Test Failed: (Case 05) Header not exists or format error")
+		return false
+	}
+
+	if response.RedirectStatus == true {
+		t.Error("Test Failed: (Case 05) Redirect should not be found here")
+		return false
+	}
+	return true
+}
+
+func testGet06(t *testing.T) bool {
+	request := Request{}
+
+	request.URL = "https://api.yellowfish.top"
+	request.Method = "GET"
+	request.NotRedirect = true
+	request.Authentication = &authentication
+
+	response, err := httpRequest(request)
+
+	if err != nil {
+		t.Error(err)
+		return false
+	}
+
+	if response.RedirectStatus == false {
+		t.Error("Test Failed: (Case 06) Redirect should be found here")
+		return false
+	}
+
+	return true
+}
+
+func Test_httpRequest(t *testing.T) {
 	status := true
-	if e != nil {
-		t.Error(e)
-		status = false
-	} else if res == "" {
-		t.Error("No Response from http://www.bing.com")
-		status = false
-	} else if GlobalCookie == nil || len(GlobalCookie) == 0 {
-		t.Error("Cookie is an empty field")
+	authentication = Authentication{}
+
+	if testPOST01(t) == false {
 		status = false
 	}
 
-	if status == false {
-		t.Log("HTTP GET Query Test Failed")
+	if testPOST02(t) == false {
+		status = false
+	}
+
+	if testPOST03(t) == false {
+		status = false
+	}
+
+	if testPOST04(t) == false {
+		status = false
+	}
+
+	if testGet05(t) == false {
+		status = false
+	}
+
+	if testGet06(t) == false {
+		status = false
+	}
+
+	if status == true {
+		t.Log("Package conn - httpRequest test passed")
 	} else {
-		t.Log("HTTP GET Query Test Passed")
+		t.Log("Package conn - httpRequest test failed")
 	}
 }
 
-func Test_httpsGET(t *testing.T) {
-	var GlobalCookie []*http.Cookie
-	res, e := HTTPGet("http://www.bing.com", &GlobalCookie, map[string]string{"Host": "www.bing.com"})
+func testQuery01(t *testing.T) bool {
+	request := Request{}
 
-	status := true
+	request.URL = "https://www.baidu.com"
+	request.Method = "GET"
+	request.NotRedirect = false
+	request.Authentication = &authentication
 
-	if e != nil {
-		t.Error(e)
-		status = false
-	} else if res == "" {
-		t.Error("No Response from http://www.bing.com")
-		status = false
-	} else if GlobalCookie == nil || len(GlobalCookie) == 0 {
-		t.Error("Cookie is an empty field")
-		status = false
+	_, err := HTTPRequest(request)
+
+	if err != nil {
+		t.Error(err)
+		return false
 	}
 
-	if status == false {
-		t.Log("HTTPS GET Query Test Failed")
-	} else {
-		t.Log("HTTPS GET Query Test Passed")
-	}
+	return true
 }
 
-func Test_httpPOST(t *testing.T) {
-	var GlobalCookie []*http.Cookie
-	res, e := HTTPPost("http://www.bing.com", &GlobalCookie, map[string]string{"Host": "www.bing.com"}, map[string]string{"hi": "hi"})
+func testQuery02(t *testing.T) bool {
+	request := Request{}
 
-	status := true
+	request.URL = "https://www.baidu.as-i/ppp"
+	request.Method = "POST"
+	request.NotRedirect = false
+	request.Authentication = &authentication
 
-	if e != nil {
-		t.Error(e)
-		status = false
-	} else if res == "" {
-		t.Error("No Response from http://www.bing.com")
-		status = false
-	} else if GlobalCookie == nil || len(GlobalCookie) == 0 {
-		t.Error("Cookie is an empty field")
-		status = false
+	_, err := HTTPRequest(request)
+
+	if err == nil {
+		t.Error("Test Failed: (Case 03) HttpRequest should throw an error here")
+		return false
 	}
 
-	if status == false {
-		t.Log("HTTP POST Query Test Failed")
-	} else {
-		t.Log("HTTP POST Query Test Passed")
-	}
+	return true
 }
 
-func Test_httpsPOST(t *testing.T) {
-	var GlobalCookie []*http.Cookie
-	res, e := HTTPPost("http://www.bing.com", &GlobalCookie, map[string]string{"Host": "www.bing.com"}, map[string]string{"hi": "hi"})
-
+func Test_HTTPRequest(t *testing.T) {
 	status := true
+	authentication = Authentication{}
 
-	if e != nil {
-		t.Error(e)
-		status = false
-	} else if res == "" {
-		t.Error("No Response from http://www.bing.com")
-		status = false
-	} else if GlobalCookie == nil || len(GlobalCookie) == 0 {
-		t.Error("Cookie is an empty field")
+	if testQuery01(t) == false {
 		status = false
 	}
 
-	if status == false {
-		t.Log("HTTPS POST Query Test Failed")
+	if testQuery02(t) == false {
+		status = false
+	}
+
+	if status == true {
+		t.Log("Package conn - HTTPRequest test passed")
 	} else {
-		t.Log("HTTPS POST Query Test Passed")
+		t.Log("Package conn - HTTPRequest test failed")
 	}
 }
