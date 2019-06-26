c0d8e540b229c5039249d00de28a4bc6853f17db
limstash <limstash.w@gmail.com>
2019-06-26 15:28:54 +0800

    update module contest

--- module/contest/contest.go
+++ module/contest/contest.go
@@ -3,113 +3,80 @@ package contest
 import (
 	"encoding/json"
 	"errors"
-	"net/http"
-	"strconv"
 
-	"github.com/bitly/go-simplejson"
 	"github.com/hytzongxuan/Codeforces-Hacker/module/conn"
+
+	. "github.com/hytzongxuan/Codeforces-Hacker/common"
 )
 
-//Contest type contains contest info
-type Contest struct {
-	ID                  int    `json:"id"`
-	Name                string `json:"name"`
-	Types               string `json:"type"`
-	Phase               string `json:"phase"`
-	Frozen              bool   `json:"frozen"`
-	DurationSeconds     int64  `json:"durationSeconds"`
-	StartTimeSeconds    int64  `json:"startTimeSeconds"`
-	RelativeTimeSeconds int64  `json:"relativeTimeSeconds"`
+func apiQueryContest(auth *Authentication, server string) (Response, error) {
+	request := Request{}
+
+	request.URL = server + "/api/contest.list?gym=false"
+	request.Method = "GET"
+	request.NotRedirect = false
+	request.Authentication = auth
+	request.Header = map[string]string{"Host": "codeforces.com"}
+
+	response, err := conn.HTTPRequest(request)
+
+	return response, err
 }
 
-//Contests type contain contests info
-type Contests struct {
-	Status string    `json:"status"`
-	Result []Contest `json:"result"`
+func apiQueryProblem(auth *Authentication, server string) (Response, error) {
+	request := Request{}
+
+	request.URL = server + "/api/problemset.problems"
+	request.Method = "GET"
+	request.NotRedirect = false
+	request.Authentication = auth
+	request.Header = map[string]string{"Host": "codeforces.com"}
+
+	response, err := conn.HTTPRequest(request)
+
+	return response, err
 }
 
-//Problem type contain problem info
-type Problem struct {
-	Index string
-	Name  string
-}
+func GetContest(auth *Authentication, server string) (Contests, error) {
+	contests := Contests{}
 
-func queryContests(cookie *[]*http.Cookie) (string, error) {
-	res, err := conn.HTTPGet("https://codeforces.com/api/contest.list?gym=false", cookie, map[string]string{"HOST": "codeforces.com"})
+	response, err := apiQueryContest(auth, server)
 
 	if err != nil {
-		return "", err
+		return contests, err
 	}
 
-	return res, nil
-}
-
-//GetContests will fetch contests list
-func GetContests(cookie *[]*http.Cookie) ([]Contest, error) {
-	data, e := queryContests(cookie)
-
-	if e != nil {
-		return nil, e
-	}
-
-	res := Contests{}
-	e = json.Unmarshal([]byte(data), &res)
-
-	if e != nil {
-		return nil, e
-	}
-
-	if res.Status != "OK" {
-		return nil, errors.New("Codeforces Return Error Response")
-	}
-
-	return res.Result, nil
-}
-
-func queryProblems(contestID int, cookie *[]*http.Cookie) ([]byte, error) {
-	res, err := conn.HTTPGetByte("https://codeforces.com/api/contest.standings?contestId="+strconv.Itoa(contestID)+"&from=1&count=1", cookie, map[string]string{"HOST": "codeforces.com"})
+	err = json.Unmarshal(response.ResponseBody, &contests)
 
 	if err != nil {
-		return nil, err
+		return contests, err
 	}
 
-	return res, nil
+	if contests.Status != "OK" {
+		return contests, errors.New("Codeforces API return unknown error")
+	}
+
+	return contests, nil
 }
 
-// GetProblems will fetch problems list from a contest
-func GetProblems(contestID int, cookie *[]*http.Cookie) ([]Problem, error) {
-	data, e := queryProblems(contestID, cookie)
+func GetProblem(auth *Authentication, server string) (Problems, error) {
+	problems := Problems{}
 
-	if e != nil {
-		return nil, e
+	response, err := apiQueryProblem(auth, server)
+
+	if err != nil {
+		return problems, err
 	}
 
-	res := []Problem{}
+	err = json.Unmarshal(response.ResponseBody, &problems)
 
-	js, e := simplejson.NewJson(data)
-
-	if e != nil {
-		return nil, e
+	if err != nil {
+		return problems, err
 	}
 
-	status, e := js.Get("status").String()
-
-	if status != "OK" {
-		return nil, errors.New("Codeforces Return Error Response")
+	if problems.Status != "OK" {
+		return problems, errors.New("Codeforces API return unknown error")
 	}
 
-	problem, e := js.Get("result").Get("problems").Array()
-
-	if e != nil {
-		return nil, e
-	}
-
-	for i := 0; i < len(problem); i++ {
-		index := problem[i].(map[string]interface{})["index"].(string)
-		name := problem[i].(map[string]interface{})["name"].(string)
-
-		res = append(res, Problem{index, name})
-	}
-
-	return res, nil
+	return problems, nil
 }
--- module/contest/contest_test.go
+++ module/contest/contest_test.go
@@ -1,103 +1,53 @@
 package contest
 
 import (
-	"net/http"
 	"testing"
+
+	. "github.com/hytzongxuan/Codeforces-Hacker/common"
 )
 
-func Test_QueryContests(t *testing.T) {
-	var GlobalCookie []*http.Cookie
-	i, e := queryContests(&GlobalCookie)
-
+func Test_GetContest(t *testing.T) {
 	status := true
+	auth := Authentication{}
 
-	if e != nil {
-		t.Error(e)
-		status = false
-	} else if i == "" {
-		t.Error("Empty Response from codeforces.com")
-		status = false
-	} else if GlobalCookie == nil || len(GlobalCookie) == 0 {
-		t.Error("Cookie is en empty field")
+	contest, err := GetContest(&auth, "https://codeforces.com")
+
+	if err != nil {
+		t.Error(err)
 		status = false
 	}
 
-	if status == false {
-		t.Log("Query Contests Test Failed")
+	if len(contest.Result) <= 0 {
+		t.Error("Test Failed: GetContest should return a non-empty contests array")
+		status = false
+	}
+
+	if status == true {
+		t.Log("Package contest - GetContest test passed")
 	} else {
-		t.Log("Query Contests Test Passed")
+		t.Log("Package contest - GetContest test failed")
 	}
 }
 
-func Test_GetContests(t *testing.T) {
-	var GlobalCookie []*http.Cookie
-	i, e := GetContests(&GlobalCookie)
-
+func Test_GetProblem(t *testing.T) {
 	status := true
+	auth := Authentication{}
 
-	if e != nil {
-		t.Error(e)
-		status = false
-	} else if i == nil {
-		t.Error("Result is an empty field")
-		status = false
-	} else if GlobalCookie == nil || len(GlobalCookie) == 0 {
-		t.Error("Cookie is en empty field")
+	problem, err := GetProblem(&auth, "https://codeforces.com")
+
+	if err != nil {
+		t.Error(err)
 		status = false
 	}
 
-	if status == false {
-		t.Log("Get Contests Test Failed")
+	if len(problem.Result.Problems) <= 0 {
+		t.Error("Test Failed: GetProblem should return a non-empty problems array")
+		status = false
+	}
+
+	if status == true {
+		t.Log("Package contest - GetProblem test passed")
 	} else {
-		t.Log("Get Contests Test Passed")
-	}
-}
-
-func Test_QueryProblems(t *testing.T) {
-	var GlobalCookie []*http.Cookie
-
-	i, e := queryProblems(556, &GlobalCookie)
-
-	status := true
-
-	if e != nil {
-		t.Error(e)
-		status = false
-	} else if i == nil || len(i) == 0 {
-		t.Error("Empty Response from codeforces.com")
-		status = false
-	} else if GlobalCookie == nil || len(GlobalCookie) == 0 {
-		t.Error("Cookie is en empty field")
-		status = false
-	}
-
-	if status == false {
-		t.Log("Query Problems Test Failed")
-	} else {
-		t.Log("Query Problems Test Passed")
-	}
-}
-
-func Test_GetProblems(t *testing.T) {
-	var GlobalCookie []*http.Cookie
-	i, e := GetProblems(556, &GlobalCookie)
-
-	status := true
-
-	if e != nil {
-		t.Error(e)
-		status = false
-	} else if i == nil || len(i) == 0 {
-		t.Error("Problems is an empty field")
-		status = false
-	} else if GlobalCookie == nil || len(GlobalCookie) == 0 {
-		t.Error("Cookie is en empty field")
-		status = false
-	}
-
-	if status == false {
-		t.Log("Get Problems Test Failed")
-	} else {
-		t.Log("Get Problems Test Passed")
+		t.Log("Package contest - GetProblem test failed")
 	}
 }
